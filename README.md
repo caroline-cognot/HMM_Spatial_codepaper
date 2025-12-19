bc327b0c80dc7335c502c8a8df4e22c7aa3f0ec2
cognot <caroline.cognot@agroparistech.fr>
2025-12-19 11:31:32 +0100

    add SpatialBernoulli code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # HMMSPAcodepaper
 Code for the paper "Spatio-temporal generation of precipitation using a Hidden Markov Model, extended extreme distributions and conditional Gaussian fields" 
+
+"data/" contains the French station data used in the application section of the paper
+
+The scripts are organised as follows :
+
+
+## fitting a Periodic Hidden Markov Model with spatially correlated emission
+* "SpatialBernoulli/" contains the code for the simple spatial emission only. 
+    * 
+* "utils/" contains code for several useful functions:
+    * 
+
+Inside each of these folders, two scripts can be of use :
+* 
---
+++ SpatialBernoulli/test_simulated_data.jl
@@ -0,0 +1,49 @@
+include("/home/caroline/Gitlab_SWG_Caro/hmmspa/SpatialBernoulli/SpatialBernoulli.jl")
+
+
+
+Random.seed!(0)
+
+
+# define locations in the unit square
+my_locations = vcat(([x y] for x in 0:0.2:1 for y in 0:0.2:1)...)
+nlocs = length(my_locations[:, 1])
+
+
+scatter(my_locations[:, 1], my_locations[:, 2])
+my_distance = [sqrt(sum(abs2, my_locations[i, :] - my_locations[j, :])) for i in axes(my_locations, 1), j in axes(my_locations, 1)]
+
+# randomly generate a SpatialBernoulli
+my_λ = rand(nlocs)# [0.5+ 0.1*i for i in 1:nlocs] 
+my_range = 0.3
+my_sill = 1.0
+my_order = 1 / 2
+d = SpatialBernoulli(my_range, my_sill, my_order, my_λ, my_distance)
+heatmap(d.ΣU)
+
+# generate data
+n = 20
+y=rand(d,n)
+PlotSim(y[:, 1:9], my_locations)
+
+
+# fit a model using initial values
+init_range = 0.5
+init_order = 1.5
+init_lambda = fill(0.4, nlocs)
+init_d = SpatialBernoulli(init_range, 1.0, init_order, init_lambda, my_distance)
+heatmap(init_d.ΣU)
+
+# using full likelihood is a bad idea.
+# @timed sol = fit_mle(init_d,y; m = 100*length(init_d), return_sol = false)
+# #maximise without the order !
+ @timed sol = fit_mle(init_d,y; m = 10*length(init_d), return_sol = false, order = my_order)
+
+# pairwise maximisation is a lot better
+tdist = maximum(my_distance) / 1
+wp = 1.0 .* (my_distance .< tdist)
+# @timed sol1 = fit_mle(init_d, y, wp; m=100 * 2, return_sol=false,maxiters=200)
+@timed sol2 = fit_mle(init_d, y, wp; order=my_order, m=30 * 2, return_sol=false, maxiters = 2000)
+# end
+
+
